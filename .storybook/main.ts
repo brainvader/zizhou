75f1382e41809e68c7573ded9a98e4dc62b9db03
brainvader <brainvade@gmail.com>
2026-05-10 17:23:09 +0900

    chore: install vitest addon for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -16,6 +16,7 @@ const config: StorybookConfig = {
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
     '@storybook/addon-onboarding',
+    '@storybook/addon-vitest'
   ],
   framework: {
     name: '@storybook/react-vite',
